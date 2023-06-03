760f4f8e5cf5916e7a20c2256579778c136bb83e
Hammas ahmad <59486644+hmhtech@users.noreply.github.com>
2023-06-03 13:14:32 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -17,10 +17,10 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('Sarfraz.so'):
-        os.system('curl -L https://github.com/Sarfraz-XD/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz32.so') 
-        import Sarfraz32
+        os.system('curl -L https://github.com/Sarfraz-XD/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
+        import Sarfraz
     else:
-        import Sarfraz32
+        import Sarfraz
 
 elif bit == '32bit':
     if not os.path.isfile('Sarfraz32.so'):
